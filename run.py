43313fdcaf78fb10f80d4108c35f655018570b03
Stuart Roeszler <stuart@roeszler.com>
2022-06-02 17:22:58 +0000

    FEAT: add summary instructions to get user data

--- run.py
+++ run.py
@@ -15,6 +15,43 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('n3orthotics')
 
-sales = SHEET.worksheet('orders')
-data = sales.get_all_values()
-print(data)
+# sales = SHEET.worksheet('orders')
+# data = sales.get_all_values()
+# print(data)
+
+
+def start():
+    """
+    Start screen prompting user to:
+    1. Create a new order, or
+    2. Retrieve an exsisting order with order number
+    """
+    print('Welcome to N(3)ORTHOTICS order portal.\n')
+    print('Use this app to directly access made-to-order N3D Printed Insoles')
+    print('Please visit northotics.com/home for more information\n')
+    print('Select 1. : Place a new N3D insole order')
+    print('Select 2. : Retrieve an exsisting N3D order')
+    print('Select 3. : Exit Program\n')
+    # select_option()
+
+
+def get_user_data():
+    """
+    Get User first name, last name and email from user as a string
+    """
+    print('Where prompted below, please enter your name and email.')
+    print('This user data should be in a valid syntax, for example:\n')
+    print('First Name: Bobby\nLast Name: Hunden')
+    print('Email: bobby123@domain.com\n')
+
+
+def main():
+    """
+    Run all program functions
+    """
+    user = get_user_data()
+    print(user)
+# main()
+
+
+get_user_data()

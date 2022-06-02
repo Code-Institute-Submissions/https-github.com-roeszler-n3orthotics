715de0e8b678027b742cdcd5f8fe661da9fac6a0
Stuart Roeszler <stuart@roeszler.com>
2022-06-02 17:50:43 +0000

    FEAT add validation user email input

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('n3orthotics')
-REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+REGEX = r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$'
 
 # sales = SHEET.worksheet('orders')
 # data = sales.get_all_values()
@@ -40,36 +40,61 @@ def start():
 def get_user_data():
     """
     Get User first name, last name and email from user as a string
+    with fist letter capitalized for names and all lowercase email 
     """
     print('Where prompted below, please enter your name and email.')
     print('This information should be in a valid syntax, with no spaces. For example:\n')
+    
     print('First Name: Bobby\nLast Name: Hunden')
     print('Email: bobby123@yourdomain.com\n')
-
-    f_name = input('Your First Name: ')
-    l_name = input('Your Last Name: ')
-    user_email = input('Your Email: ')
-
-    print(f'\nThanks {remove(f_name.capitalize())}. Your user details are as follows:')
+    
+    f_name = remove(input('Your First Name: ').capitalize())
+    l_name = remove(input('Your Last Name: ').capitalize())
+    user_email = remove(input('Your Email: ').lower())
+    
+    print(f'\nThanks {f_name}. Your user details are as follows:')
     print('------------')
-    print(f'Full Name: {remove(f_name.capitalize())} {remove(l_name.capitalize())}\nEmail: {remove(user_email.lower())}')
+    print(f'Full Name: {f_name} {l_name}\nEmail: {user_email}')
     print('------------\n')
+    # validate_user_names(f'{f_name},{l_name}')
+    validate_user_email(f'{user_email}')
     yes_no_user()
-    validate_user_data(f'{remove(f_name.capitalize())}, {l_name.capitalize()}, {user_email.lower()}')
+
+
+    # user_data = [f_name, l_name, user_email]
+    # print(user_data)
+
+def validate_user_email(values):
+    """
+    Inside the try, checks all user_email input syntax.
+    Raises ValueError if strings cannot be converted
+    """
+    values_string = f'{values.split(",")}'
+    print(f'The user_data you provided converted into a list of strings is:\n{values_string}\n')
+
+    try:
+        if (re.fullmatch(REGEX, values)):
+            print('valid email')
+        else:
+            # print('invalid email')
+            raise ValueError(
+                f'The email you have provided "{values}" does not seem\nto be in a regular format'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}. Please check the entry and try again.\n')
+
 
 def remove(string):
+    """
+    Removes all spaces in string inputs
+    """
     return string.replace(' ', '')
 
-def validate_user_data(values):
-    """
-    Inside the try, converts all user_email inputs into floats.
-    Raises ValueError if strings cannot be converted into float or
-    if height == low or mid or high string values or 
-    if width == narrow or standard or wide.
-    """
-    print(f'The data you provided converted into a list of strings is:\n{remove(values)}\n')
 
-
+def get_latest_row_entry():
+    orders = SHEET.worksheet('orders').get_values('A:F')
+    email = orders[-1]
+    print(email)
 
 def yes_no_user():
     correct = input('Is this information correct? y/n: ').lower()
@@ -77,12 +102,13 @@ def yes_no_user():
         # print(f'Thanks *** , updating worksheet and proceeding to order_data\n')
         return True
     else:
-        get_user_data()
-
-
+        main()
 def main():
     """
     Run all program functions
     """
     user = get_user_data()
 main()
+
+# get_latest_row_entry()
+# validate_user_data()

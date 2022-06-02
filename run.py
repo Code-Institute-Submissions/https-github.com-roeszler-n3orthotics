63e7f1a273e21d06ea662aa666d4a5fb5e118d39
Stuart Roeszler <stuart@roeszler.com>
2022-06-02 17:41:09 +0000

    FEAT add validation user data input

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ Main running file n(3)orthotics order portal
 """
 import gspread
 from google.oauth2.service_account import Credentials
+import re
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -14,6 +15,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('n3orthotics')
+REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 # sales = SHEET.worksheet('orders')
 # data = sales.get_all_values()
@@ -48,10 +50,35 @@ def get_user_data():
     l_name = input('Your Last Name: ')
     user_email = input('Your Email: ')
 
-    print(f'\nThanks {f_name}. Your user details are as follows:')
+    print(f'\nThanks {remove(f_name.capitalize())}. Your user details are as follows:')
     print('------------')
-    print(f'Full Name: {f_name.capitalize()} {l_name.capitalize()}\nEmail: {user_email.lower()}')
+    print(f'Full Name: {remove(f_name.capitalize())} {remove(l_name.capitalize())}\nEmail: {remove(user_email.lower())}')
     print('------------\n')
+    yes_no_user()
+    validate_user_data(f'{remove(f_name.capitalize())}, {l_name.capitalize()}, {user_email.lower()}')
+
+def remove(string):
+    return string.replace(' ', '')
+
+def validate_user_data(values):
+    """
+    Inside the try, converts all user_email inputs into floats.
+    Raises ValueError if strings cannot be converted into float or
+    if height == low or mid or high string values or 
+    if width == narrow or standard or wide.
+    """
+    print(f'The data you provided converted into a list of strings is:\n{remove(values)}\n')
+
+
+
+def yes_no_user():
+    correct = input('Is this information correct? y/n: ').lower()
+    if correct.startswith('y'):
+        # print(f'Thanks *** , updating worksheet and proceeding to order_data\n')
+        return True
+    else:
+        get_user_data()
+
 
 def main():
     """
